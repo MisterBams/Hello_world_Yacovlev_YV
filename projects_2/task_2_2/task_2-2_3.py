3b4061a0c490984e0003997a7d1d3931557251bb
Яковлев Юрий Вадимович <yurayacoff@yandex.ru>
2026-02-23 21:53:18 +0300

    Add files via upload

---
+++ projects_2/task_2_2/task_2-2_3.py
@@ -0,0 +1,6 @@
+thing_name = "Осциллограф"
+thing_num = 2718281828459045
+thing_cond = "Stable"
+thing_amount = 23
+
+print(f"Название:\t{thing_name}\nИнв.номер:\t{thing_num}\nCостояние:\t{thing_cond}\nКол-во:\t\t{thing_amount}")
