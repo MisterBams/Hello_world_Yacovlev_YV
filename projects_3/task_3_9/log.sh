658c7e4a4c9f14f619099771980abbcf14bf6062
Яковлев Юрий Вадимович <yurayacoff@yandex.ru>
2026-03-19 10:13:26 +0300

    Update log.sh

--- projects_3/task_3_9/log.sh
+++ projects_3/task_3_9/log.sh
@@ -3,14 +3,12 @@
 FILE_PATH="./system.log"
 ERROR_CODE=1
 
-# 1. Файловый тест
 if [ -f "$FILE_PATH" ]; then
     echo "Лог-файл найден."
 else
     echo "Ошибка: файл не существует."
 fi
 
-# 2. Числовое сравнение через Case
 case $ERROR_CODE in
     0)
         echo "Статус: Ошибок нет." ;;
